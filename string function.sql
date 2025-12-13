9fe61048e8381f14977e5b6cc3e1899f3b778ce5
swapnilmane221-coder <swapnilmane2521@gmail.com>
2025-12-13 22:45:53 +0530

    string function

---
+++ string function.sql
@@ -0,0 +1,18 @@
+select upper('hello');
+
+select lower('SWAPNIL');
+
+select length('swapnil');
+
+select concat('hello',' world');
+
+select substring('PostgreSQL',8,11);
+
+select trim('        SQL   ');
+
+select replace('SQL Query','Query','Command');
+
+select left('swapnil',5);
+
+select right('swapnil',3);
+
