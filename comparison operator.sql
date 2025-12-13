decb52071a20a0e994ddd31ccd95eea75a86d91d
swapnilmane221-coder <swapnilmane2521@gmail.com>
2025-12-13 12:30:44 +0530

    comparison operator

---
+++ comparison operator.sql
@@ -0,0 +1,13 @@
+select * from customers;
+
+select * from customers where age=30;
+
+select * from customers where age!=30;
+
+select * from customers where age>30;
+
+select * from customers where age<30;
+
+select * from customers where age>=17;
+
+select * from customers where age<=35;
