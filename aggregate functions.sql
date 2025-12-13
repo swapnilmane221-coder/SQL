4cc6df35174e1bb132f25723b3d0868df0579adb
swapnilmane221-coder <swapnilmane2521@gmail.com>
2025-12-13 14:47:34 +0530

    aggregate function

---
+++ aggregate functions.sql
@@ -0,0 +1,16 @@
+select * from employees;
+
+-- count 
+select count(*) from employees;
+
+-- sum
+select sum(salary) from employees;
+
+-- avg 
+select avg(salary),avg(age) from employees;
+
+-- max
+select max(salary),max(age) from employees;
+
+-- min
+select min(salary),min(age) from employees;
